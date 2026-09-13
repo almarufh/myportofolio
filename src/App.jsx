a1aae5a8065146d707f1fda132e89e8c21d5ce46
almarufh_ <hidayatmaruf99@gmail.com>
2026-09-13 09:13:48 +0700

    feat: import and use aos

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css"; // Import CSS bawaan AOS
+
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import About from "./components/About"
@@ -6,6 +10,14 @@ import Contact from "./components/Contact"
 import Footer from "./components/Footer"
 
 function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: true,
+      offset: 100,
+    });
+  }, []);
+
   return (
     <div className="bg-zinc-900 text-white min-h-screen">
       <Navbar />
