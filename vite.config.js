65d4fb18f978c7a665a989b378271b585ce0c94f
almarufh_ <hidayatmaruf99@gmail.com>
2026-09-13 09:20:19 +0700

    chore: update vite base url for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
+  // Menyesuaikan dengan nama repository GitHub Anda
+  base: '/myportofolio/',
 })
